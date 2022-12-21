a374f273f9c7112ac894feb8ec55923d4fe66667
arnaud chevallier <chevallier.a0@gmail.com>
2022-12-21 13:23:22 +0100

    update navbar responsive all good, changed some styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,9 +12,8 @@ const Navbar = () => {
 
         return (
                 <nav className="navbar navbar-expand-lg" style={{backgroundColor: darkMode ? '#222' : 'wheat',
-                        color: darkMode ? 'wheat' : '#222'}}>
+                        color: darkMode ? 'wheat' : '#222', borderBottom: darkMode ? "1px solid wheat" : '1px solid #222'}}>
 
-                <div className="container">
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <FontAwesomeIcon icon={ faBars } style={{ color: darkMode ? "Antiquewhite" : "#222", backgroundColor: darkMode ? "#222" : "wheat"}}/>
@@ -30,38 +29,37 @@ const Navbar = () => {
 				</div>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{backgroundColor: darkMode ? '#222' : 'wheat',
-                        borderBottom: darkMode ? "1px solid wheat" : '1px solid #222'}}>
-                <ul className="navbar-nav ml-auto" style={{backgroundColor: darkMode ? '#222' : 'wheat',
+                        }}>
+                    <ul className="navbar-nav ml-auto" style={{backgroundColor: darkMode ? '#222' : 'wheat',
                             color: darkMode ? 'wheat' : '#222'}}>
-                    <li className="nav-item" style={{backgroundColor: darkMode ? '#222' : 'wheat',
-                            color: darkMode ? 'wheat' : '#222'}}>
-                    <a className="nav-links" aria-current="page" href="#aboutId" style={{backgroundColor: darkMode ? '#222' : 'wheat',
-                            color: darkMode ? 'wheat' : '#222'}}>About</a>
-                    </li>
-                    <li className="nav-item" style={{backgroundColor: darkMode ? '#222' : 'wheat',
-                            color: darkMode ? 'wheat' : '#222'}}>
-                    <a className="nav-links" href="#experiencesId" style={{backgroundColor: darkMode ? '#222' : 'wheat',
-                            color: darkMode ? 'wheat' : '#222'}}>Experiences</a>
-                    </li>
-                    <li className="nav-item" style={{backgroundColor: darkMode ? '#222' : 'wheat',
-                            color: darkMode ? 'wheat' : '#222'}}>
-                    <a className="nav-links" href="#servicesId" style={{backgroundColor: darkMode ? '#222' : 'wheat',
-                            color: darkMode ? 'wheat' : '#222'}}>Services</a>
-                    </li>
-                    <li className="nav-item" style={{backgroundColor: darkMode ? '#222' : 'wheat',
-                            color: darkMode ? 'wheat' : '#222'}}>
-                    <a className="nav-links" href="#portfolioId" style={{backgroundColor: darkMode ? '#222' : 'wheat',
-                            color: darkMode ? 'wheat' : '#222'}}>Portfolio</a>
-                    </li>
-                    <li className="nav-item" style={{backgroundColor: darkMode ? '#222' : 'wheat',
-                            color: darkMode ? 'wheat' : '#222'}}>
-                    <a className="nav-links" href="#contactId" style={{backgroundColor: darkMode ? '#222' : 'wheat',
-                            color: darkMode ? 'wheat' : '#222'}}>Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        </nav>
+                        <li className="nav-item" style={{backgroundColor: darkMode ? '#222' : 'wheat',
+                                color: darkMode ? 'wheat' : '#222'}}>
+                        <a className="nav-links" aria-current="page" href="#aboutId" style={{backgroundColor: darkMode ? '#222' : 'wheat',
+                                color: darkMode ? 'wheat' : '#222'}}>About</a>
+                        </li>
+                        <li className="nav-item" style={{backgroundColor: darkMode ? '#222' : 'wheat',
+                                color: darkMode ? 'wheat' : '#222'}}>
+                        <a className="nav-links" href="#experiencesId" style={{backgroundColor: darkMode ? '#222' : 'wheat',
+                                color: darkMode ? 'wheat' : '#222'}}>Experiences</a>
+                        </li>
+                        <li className="nav-item" style={{backgroundColor: darkMode ? '#222' : 'wheat',
+                                color: darkMode ? 'wheat' : '#222'}}>
+                        <a className="nav-links" href="#servicesId" style={{backgroundColor: darkMode ? '#222' : 'wheat',
+                                color: darkMode ? 'wheat' : '#222'}}>Services</a>
+                        </li>
+                        <li className="nav-item" style={{backgroundColor: darkMode ? '#222' : 'wheat',
+                                color: darkMode ? 'wheat' : '#222'}}>
+                        <a className="nav-links" href="#portfolioId" style={{backgroundColor: darkMode ? '#222' : 'wheat',
+                                color: darkMode ? 'wheat' : '#222'}}>Portfolio</a>
+                        </li>
+                        <li className="nav-item" style={{backgroundColor: darkMode ? '#222' : 'wheat',
+                                color: darkMode ? 'wheat' : '#222'}}>
+                        <a className="nav-links" href="#contactId" style={{backgroundColor: darkMode ? '#222' : 'wheat',
+                                color: darkMode ? 'wheat' : '#222'}}>Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                </nav>
     )
 }
 
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -9,35 +9,32 @@ const Presentation = () => {
     const theme = useContext(ThemeContext);
     const darkMode = theme.state.darkMode;
 
-  return (
-    <>
-    <div className='presentation' style={{background: darkMode ? "linear-gradient(100deg, #222 0%, wheat 50%, #222 100%)" : ""}}>
-        <div className='presentation-title'>
-            <p className='hello' style={{color: darkMode ? '#222' : 'wheat'}}>Hello, my name is</p>
-            <h1 className='name' style={{color: darkMode ? '#222' : 'wheat'}}>Arnaud</h1>
-        </div>
-    
-        <div className="title" style={{color: darkMode ? '#222' : 'wheat'}}>
-            <div className="title-wrapper">
-              <div className="title-item">Web Developer</div>
-              <div className="title-item">Software Engineer</div>
-              <div className="title-item">UI-UX Designer</div>
+    return (
+        <>
+        <div className='presentation' style={{background: darkMode ? "linear-gradient(100deg, #222 0%, wheat 50%, #222 100%)" : ""}}>
+            <div className='presentation-title'>
+                <p className='hello' style={{color: darkMode ? '#222' : 'wheat'}}>Hello, my name is</p>
+                <h1 className='name' style={{color: darkMode ? '#222' : 'wheat'}}>Arnaud</h1>
+            </div>
+            <div className="title" style={{color: darkMode ? '#222' : 'wheat'}}>
+                <div className="title-wrapper">
+                    <div className="title-item">Web Developer</div>
+                    <div className="title-item">Software Engineer</div>
+                    <div className="title-item">UI-UX Designer</div>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div className="header-wraper">
-
+        <div className="header-wraper">
             <div className="main-info">
-
                 <Typed
                     className="typed"
                     strings={[
-                        ' Web Integration',
-                        ' Web Design',
-                        ' Web Development',
-                        ' Web applications',
-                        ' Databases'
+                            ' Web Integration',
+                            ' Web Design',
+                            ' Web Development',
+                            ' Web applications',
+                            ' Databases'
                     ]}
                     typeSpeed={40}
                     backSpeed={50}
@@ -45,14 +42,11 @@ const Presentation = () => {
                     backDelay={1000}
                 />
                 <br/>
-        
                 <a href="#contactId" className="btn-contact">Contact me now</a>
-            
             </div>
-            
         </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Presentation
